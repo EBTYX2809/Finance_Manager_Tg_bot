1c9c04c2703458ca532ab09c123b2e94a85d979c
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-06-06 20:29:49 +0300

    Created architecture of project.

---
+++ Finance_Manager_Tg_bot/BackendApi/ApiClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance_Manager_Tg_bot.BackendApi;
+
+public class ApiClient
+{
+}
