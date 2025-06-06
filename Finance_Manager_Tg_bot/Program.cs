e1c852cdf3ff0e360eaabf3747a6fab300a4c836
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-06-06 20:20:43 +0300

    Init

---
+++ Finance_Manager_Tg_bot/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Telegram.Bot;
+
+namespace Finance_Manager_Tg_bot;
+
+public class Program
+{
+    private static string apiKey;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        apiKey = config["TelegramApiKey"];
+
+        StartBot().Wait();
+    }
+
+    public static async Task StartBot()
+    {        
+        var bot = new TelegramBotClient(apiKey);
+        var me = await bot.GetMe();
+        Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+    }
+}
