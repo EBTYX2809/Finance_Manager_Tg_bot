5978f129874964cb6f5286fc00b3abead35b28ee
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-06-12 06:44:35 +0300

    Created all api call methods.

--- Finance_Manager_Tg_bot/BackendApi/ApiClient.cs
+++ Finance_Manager_Tg_bot/BackendApi/ApiClient.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace Finance_Manager_Tg_bot.BackendApi;
@@ -47,11 +48,43 @@ public class ApiClient
         {
             var errorContent = await response.Content.ReadAsStringAsync();
             var error = JsonSerializer.Deserialize<ErrorResponse>(errorContent);
+            _logger.LogError(error.Message, "Error in BackendApi.");
+            throw new ApiException(error);
+        }
+    }
+
+    public async Task<bool> HandleResponseAsync(HttpResponseMessage response)
+    {
+        if (response.IsSuccessStatusCode)
+        {
+            return true;
+        }
+        else
+        {
+            var errorContent = await response.Content.ReadAsStringAsync();
+            var error = JsonSerializer.Deserialize<ErrorResponse>(errorContent);
+            _logger.LogError(error.Message, "Error in BackendApi.");
             throw new ApiException(error);
         }
     }
 
     // Auth endpoints
+    #region
+    public async Task<AuthUserTokensDTO> RegisterAsync(string email, string password)
+    {
+        AuthDataDTO data = new AuthDataDTO
+        {
+            email = email,
+            password = password
+        };
+
+        var content = GetStringContent(data);
+
+        var response = await _httpClient.PostAsync("auth/register", content);
+
+        return await HandleResponseAsync<AuthUserTokensDTO>(response);
+    }
+
     public async Task<AuthUserTokensDTO> AuthenticateAsync(string email, string password)
     {
         AuthDataDTO data = new AuthDataDTO
@@ -69,6 +102,72 @@ public class ApiClient
 
     public async Task<AuthUserTokensDTO> RefreshTokenAsync(string refreshToken)
     {
-        return null;
+        var content = GetStringContent(refreshToken);
+
+        var response = await _httpClient.PostAsync("auth/refresh-token", content);
+
+        return await HandleResponseAsync<AuthUserTokensDTO>(response);
     }
+    #endregion
+
+    // User endpoints
+    #region
+    public async Task<UserBalanceDTO> GetBalanceAsync(int id)
+    {
+        var response = await _httpClient.GetAsync($"user/{id}");
+
+        return await HandleResponseAsync<UserBalanceDTO>(response);
+    }
+
+    public async Task<bool> AddTelegramIdToUserAsync(int userId, long telegramId)
+    {
+        UserIdTelegramIdDTO data = new UserIdTelegramIdDTO
+        {
+            UserId = userId,
+            TelegramId = telegramId           
+        };
+
+        var content = GetStringContent(data);
+
+        var request = new HttpRequestMessage(HttpMethod.Put, "user/telegramId");
+        request.Content = content;
+
+        var accessToken = await _tokensManager.GetAccessTokenAsync();
+
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+        var response = await _httpClient.SendAsync(request);
+
+        return await HandleResponseAsync(response);
+    }
+    #endregion
+
+    // Transaction endpoints
+    #region
+    public async Task<bool> CreateTransactionAsync(TransactionDTO transaction)
+    {
+        var content = GetStringContent(transaction);
+
+        var request = new HttpRequestMessage(HttpMethod.Post, "transactions");
+        request.Content = content;
+
+        var accessToken = await _tokensManager.GetAccessTokenAsync();
+
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+        var response = await _httpClient.SendAsync(request);
+
+        return await HandleResponseAsync(response);
+    }
+    #endregion
+
+    // Category endpoints
+    #region
+    public async Task<List<CategoryDTO>> GetAllCategoriesAsync()
+    {       
+        var response = await _httpClient.GetAsync("categories");
+
+        return await HandleResponseAsync<List<CategoryDTO>>(response);
+    }
+    #endregion
 }
