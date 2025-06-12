abfcc77a09d4e5d13b427d2d0251eb284e2617e5
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-06-12 06:43:45 +0300

    Created CategoriesStorage, similar as from WPF front.

---
+++ Finance_Manager_Tg_bot/Models/CategoriesStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance_Manager_Tg_bot.Models;
+
+public static class CategoriesStorage
+{
+    public static List<CategoryDTO> AllCategories { get; set; } = new();
+
+    public static CategoryDTO? GetCategoryById(int id)
+    {
+        return AllCategories.FirstOrDefault(c => c.Id == id);
+    }    
+}
